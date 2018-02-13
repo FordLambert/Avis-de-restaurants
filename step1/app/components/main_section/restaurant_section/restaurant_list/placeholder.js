f1982b9d2b1399ef14d34739166558f64d359b6f
ford_lambert <ford.lambert@protonmail.ch>
2018-02-13 22:38:10 +0100

    added placeholder for restaurant list too

---
+++ step1/app/components/main_section/restaurant_section/restaurant_list/placeholder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Placeholder = () => (
+    <div className={'col-10 text-center'}>
+        <h2 className={'align-self-center'}>Pas de restaurants à afficher</h2>
+    </div>
+);
+
+export default Placeholder;
