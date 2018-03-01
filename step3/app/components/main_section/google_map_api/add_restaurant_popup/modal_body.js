82d7aae6a1abe9678f572e4aad8002f576a50d92
ford_lambert <ford.lambert@protonmail.ch>
2018-03-01 15:03:46 +0100

    bugFix: correct reset of new restaurant modal

--- step3/app/components/main_section/google_map_api/add_restaurant_popup/modal_body.js
+++ step3/app/components/main_section/google_map_api/add_restaurant_popup/modal_body.js
@@ -25,6 +25,8 @@ export default class ModalBody extends Component {
         event.preventDefault();
         //exit modal window
         window.location = '#!';
+         //reset input value
+         this.reviewForm.reset();
         //give the name to parent component
         this.props.handleSubmit(this.state.restaurantName);
     }
@@ -33,7 +35,7 @@ export default class ModalBody extends Component {
         return (
             <div className={'modal-body'}>
                 <div className="row justify-content-center">
-                    <form onSubmit={this.handleSubmit.bind(this)}>
+                    <form onSubmit={this.handleSubmit.bind(this)} ref={(element) => this.reviewForm = element}>
                         <div className="form-group text-center">
                             <Input
                                 className={'form-control'}
