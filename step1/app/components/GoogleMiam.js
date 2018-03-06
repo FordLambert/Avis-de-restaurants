02f5fa6d9ad2677a7a6ee0b838eabe16144c0755
ford_lambert <ford.lambert@protonmail.ch>
2018-03-06 17:17:22 +0100

    Made MapRestaurantList stateless and gave his job to GoogleMiam

--- step1/app/components/GoogleMiam.js
+++ step1/app/components/GoogleMiam.js
@@ -8,17 +8,106 @@ class GoogleMiam extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            grade: {},
-            order: ''
+            'listComplete': [],
+            'listCustom': [],
+            'restaurantRequested': {},
+            'position': {}
         };
+    }
+
+    getDistance(lat1, lon1, lat2, lon2) {
+        //convert lat 1 and 2 from degree to radians
+        const radlat1 = Math.PI * lat1/180;
+        const radlat2 = Math.PI * lat2/180;
+
+        //theta determine an angle between long 1 and 2
+        const theta = lon1-lon2;
+        //then convert it to radians
+        const radtheta = Math.PI * theta/180;
+
+        //multiply the sinus of (radians) lats, add the product of the long's (radians) cosine multiply by radtheta
+        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+
+        //dist equal the cosine arc between our points
+        dist = Math.acos(dist);
+
+        //convert it back from radians to degree
+        dist = dist * 180/Math.PI;
+        dist = dist * 60 * 1.1515;
+
+        //finally convert it to kilometers
+        return dist * 1.609344;
+    }
+
+    getAverageGrade(restaurant) {
+        const reviewNumber = restaurant.ratings.length;
+        let total = 0;
+        let finalGrade = 0;
+
+        if (reviewNumber > 0) {
+            
+            restaurant.ratings.map(function(restaurantReview) {
+                total += restaurantReview.stars;
+            });
+
+            finalGrade = Math.round((total/reviewNumber) * 100) / 100;
+        }
+        return finalGrade;
     }
 
     //handle the form's submit for custom restaurant options
     handleUserChoicesSubmit = (grade, order) => {
-        this.setState({
-            grade: grade,
-            order: order
+        const newList = [];
+        this.state.listComplete.map((restaurant) => {
+            const overallGrade = this.getAverageGrade(restaurant);
+
+            if ((overallGrade >= grade.min) && (overallGrade <= grade.max)) {
+                newList.push(restaurant);
+            }
         });
+
+        //sort the newly created custom array
+        //sort array by distance
+        if (order == 'distance') {
+            newList.sort((a, b) => {
+                const distA = this.getDistance(this.state.position.lat, this.state.position.lng, a.lat, a.long);
+                const distB = this.getDistance(this.state.position.lat, this.state.position.lng, b.lat, b.long);
+                return distA - distB;
+            });
+
+        //sort array by averageGrade
+        } else if (order == 'grade') {
+
+            newList.sort((a, b) => {
+                return this.getAverageGrade(b) - this.getAverageGrade(a);
+            });
+
+        //handle wrong parameter
+        } else {
+            console.log('Error: list order must be "distance" or "grade"')
+        }
+
+        this.setState({listCustom: newList});
+    }
+
+    handleMarkerClick = (restaurant) => {
+        this.setState({
+            restaurantRequested: restaurant
+        });
+    }
+
+    handleMapLoad = (geolocCoordinates) => {
+        fetch('./app/data/restaurant_list.json')
+            .then(result => {
+                return result.json();
+            })
+            .then(data => {
+                this.setState({
+                    listComplete: data,
+                    listCustom: data,
+                    position: geolocCoordinates
+                });
+            });
     }
 
     render() {
@@ -28,8 +117,10 @@ class GoogleMiam extends Component {
                     handleUserChoicesSubmit={this.handleUserChoicesSubmit}
                 />
                 <MapRestaurantList
-                    grade={this.state.grade}
-                    order={this.state.order}
+                    handleMarkerClick={this.handleMarkerClick}
+                    handleMapLoad={this.handleMapLoad}
+                    restaurantList={this.state.listCustom}
+                    restaurantRequested={this.state.restaurantRequested}
                 />
             </div>
         );
--- step1/app/components/mapRestaurantList/MapRestaurantList.js
+++ step1/app/components/mapRestaurantList/MapRestaurantList.js
@@ -6,112 +6,20 @@ import SearchResultFound from './SearchResultsFound/SearchResultsFound';
 import RestaurantSection from './RestaurantSection/RestaurantSection';
 
 export default class MapRestaurantList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'listComplete': [],
-            'listCustom': [],
-            'restaurantRequested': {},
-            'position': {}
-        };
-    }
 
     static propTypes = {
-        grade: PropTypes.object,
-        order: PropTypes.string
-    }
-
-    getDistance(lat1, lon1, lat2, lon2) {
-        //convert lat 1 and 2 from degree to radians
-        const radlat1 = Math.PI * lat1/180;
-        const radlat2 = Math.PI * lat2/180;
-
-        //theta determine an angle between long 1 and 2
-        const theta = lon1-lon2;
-        //then convert it to radians
-        const radtheta = Math.PI * theta/180;
-
-        //multiply the sinus of (radians) lats, add the product of the long's (radians) cosine multiply by radtheta
-        let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-
-        //dist equal the cosine arc between our points
-        dist = Math.acos(dist);
-
-        //convert it back from radians to degree
-        dist = dist * 180/Math.PI;
-        dist = dist * 60 * 1.1515;
-
-        //finally convert it to kilometers
-        return dist * 1.609344;
-    }
-
-    getAverageGrade(restaurant) {
-        const reviewNumber = restaurant.ratings.length;
-        let total = 0;
-        let finalGrade = 0;
-
-        if (reviewNumber > 0) {
-            
-            restaurant.ratings.map(function(restaurantReview) {
-                total += restaurantReview.stars;
-            });
-
-            finalGrade = Math.round((total/reviewNumber) * 100) / 100;
-        }
-        return finalGrade;
-    }
-
-    componentWillReceiveProps(nextProps) {
-        //create a new custom list based on user choices
-        const newList = [];
-        this.state.listComplete.map((restaurant) => {
-            const overallGrade = this.getAverageGrade(restaurant);
-
-            if ((overallGrade >= nextProps.grade.min) && (overallGrade <= nextProps.grade.max)) {
-                newList.push(restaurant);
-            }
-        });
-
-        //sort the newly created custom array
-        //sort array by distance
-        if (nextProps.order == 'distance') {
-            newList.sort((a, b) => {
-                const distA = this.getDistance(this.state.position.lat, this.state.position.lng, a.lat, a.long);
-                const distB = this.getDistance(this.state.position.lat, this.state.position.lng, b.lat, b.long);
-                return distA - distB;
-            });
-
-        //sort array by averageGrade
-        } else if (nextProps.order == 'grade') {
-
-            newList.sort((a, b) => {
-                return this.getAverageGrade(b) - this.getAverageGrade(a);
-            });
-
-        //handle wrong parameter
-        } else {
-            console.log('Error: list order must be "distance" or "grade"')
-        }
-
-        this.setState({listCustom: newList});
+        handleMarkerClick: PropTypes.func,
+        handleMapLoad: PropTypes.func,
+        restaurantList: PropTypes.array,
+        restaurantRequested: PropTypes.object,
     }
 
     handleMarkerClick = (restaurant) => {
-        this.setState({restaurantRequested: restaurant});
+        this.props.handleMarkerClick(restaurant);
     }
 
     handleMapLoad = (geolocCoordinates) => {
-        fetch('./app/data/restaurant_list.json')
-            .then(result => {
-                return result.json();
-            })
-            .then(data => {
-                this.setState({
-                    listComplete: data,
-                    listCustom: data,
-                    position: geolocCoordinates
-                });
-            });
+        this.props.handleMapLoad(geolocCoordinates);
     }
 
     render() {
@@ -119,16 +27,16 @@ export default class MapRestaurantList extends Component {
             <section className='col-12 col-md-9 col-xl-10 main-section' id='main-section'>
                 <div className='row'>
                     <GoogleMap
-                        restaurantList={this.state.listCustom}
+                        restaurantList={this.props.restaurantList}
                         handleMapLoad={this.handleMapLoad}
                         handleMarkerClick={this.handleMarkerClick}
                     />
                     <SearchResultFound
-                        restaurantNumber={this.state.listCustom.length}
+                        restaurantNumber={this.props.restaurantList.length}
                     />
                     <RestaurantSection
-                        restaurantList={this.state.listCustom}
-                        restaurantRequested={this.state.restaurantRequested}
+                        restaurantList={this.props.restaurantList}
+                        restaurantRequested={this.props.restaurantRequested}
                     />
                 </div>
             </section>
