1cf24a3341248b1dde6d6b9f7934f5e2d9816d34
ford_lambert <ford.lambert@protonmail.ch>
2018-03-06 16:33:37 +0100

    Merged Map and GoogleMap components

--- step1/app/components/mapRestaurantList/GoogleMap/GoogleMap.js
+++ step1/app/components/mapRestaurantList/GoogleMap/GoogleMap.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import Map from './Map';
 import Pulser from './LoadingPulser';
 
 export default class GoogleMap extends Component {
@@ -12,6 +11,15 @@ export default class GoogleMap extends Component {
             startPosition: {lat: 48.853, lng: 2.35},
             zoom: 12
         }
+
+        this.markerIconsPath = {
+            defaultMarkerIcon: './resources/pictures/marker-red.png',
+            geolocalisationMarkerIcon: './resources/pictures/marker-blue.png',
+            clickedMarkerIcon: './resources/pictures/marker-green.png'
+        }
+
+        this.markers = []; //markers displayed on map
+        this.infoWindows = []; //infoWindows displayed on map
     }
 
     static propTypes = {
@@ -20,25 +28,96 @@ export default class GoogleMap extends Component {
         handleMarkerClick: PropTypes.func
     }
 
+    addMarker(position, restaurant) {
+        const marker = new google.maps.Marker({
+            position: position,
+            icon: this.markerIconsPath.defaultMarkerIcon,
+            map: this.map
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: restaurant.restaurantName
+        });
+
+        marker.addListener('click', () => {
+            this.onMarkerClick(marker, restaurant, infoWindow);
+        });
+
+        this.markers.push(marker);
+        this.infoWindows.push(infoWindow);
+	}
+
+	closeInfoWindows() {
+        this.infoWindows.map((infoWindow) => {
+            infoWindow.close();
+        });
+    }
+
+    deleteOldMarkers() {
+        this.setMapOnAll(null);
+        this.markers.splice(1);
+    }
+
+    setMapOnAll(map) {
+        for (let i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(map);
+        }
+    }
+
+    onMarkerClick = (marker, restaurant, infoWindow) => {
+        this.props.handleMarkerClick(restaurant);
+        window.location = '#review-list';
+
+        this.markers.map((marker) => {
+            marker.setIcon(this.markerIconsPath.defaultMarkerIcon);
+            this.closeInfoWindows();
+        });
+
+        marker.setIcon(this.markerIconsPath.clickedMarkerIcon);
+        infoWindow.open(this.map, marker);
+    }
+
     handleMapLoad = (geolocCoordinates) => {
         this.props.handleMapLoad(geolocCoordinates);
     }
 
-    handleMarkerClick = (restaurant) => {
-        window.location = '#review-list';
-        this.props.handleMarkerClick(restaurant);
+    componentWillUpdate(nextProps) {
+        if (nextProps.restaurantList != this.props.restaurantList) {
+            this.deleteOldMarkers();
+            nextProps.restaurantList.map((restaurant) => {
+                const position = {lat: restaurant.lat, lng: restaurant.long};
+                this.addMarker(position, restaurant);
+            });
+        }
+    }
+    
+    componentDidMount() {
+        this.map = new google.maps.Map(document.getElementById('map'), {
+        	center: this.mapOptions.startPosition,
+       		zoom: this.mapOptions.zoom
+        });
+
+        navigator.geolocation.getCurrentPosition((position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            const marker = new google.maps.Marker({
+                position: pos,
+                icon: this.markerIconsPath.geolocalisationMarkerIcon,
+                map: this.map
+            });
+
+            this.map.setCenter(pos);
+            this.handleMapLoad(pos);
+        });
     }
 
     render() {
         return (
             <div id='map'>
                 <Pulser />
-                <Map
-                    mapOptions={this.mapOptions}
-                    list={this.props.restaurantList}
-                    handleMapLoad={this.handleMapLoad}
-                    handleMarkerClick={this.handleMarkerClick}
-                />
             </div>
         );
     }
